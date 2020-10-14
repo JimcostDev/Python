91baac7f80f6ef6ac13d7d6f51108928741851c4
JimcostDev <rjak727@gmail.com>
2020-10-14 12:20:08 -0500

    Tipo de datos str

---
+++ Fundamentos/2. Variables/04_TipoString.py
@@ -0,0 +1,11 @@
+cadena = "Ronaldo Jiménez Acosta"
+print("Hola, soy: {}".format(cadena))
+
+numero1 = "1"
+numero2 = "2"
+print("contacatenacion: " + numero1 + numero2)
+
+num1 = 1
+num2 = 2
+
+print("operacion suma: {}".format(num1 + num2))
